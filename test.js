66b6ca3d1df5c06970af379bba974a57b69837d6
Xihua Duan <xihua.duan@gmail.com>
2018-12-05 12:21:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto'), ecKeyUtils = require('./index.js');
 const curveNames = ['prime192v1', 'prime192v2', 'prime192v3', 'prime239v1', 'prime239v2', 'prime239v3', 'prime256v1', 'secp112r1', 'secp112r2', 'secp128r1', 'secp128r2', 'secp160k1', 'secp160r1', 'secp160r2', 'secp192k1', 'secp224k1', 'secp224r1', 'secp256k1', 'secp384r1', 'secp521r1', 'sect113r1', 'sect113r2', 'sect131r1', 'sect131r2', 'sect163k1', 'sect163r1', 'sect163r2', 'sect193r1', 'sect193r2', 'sect233k1', 'sect233r1', 'sect239k1', 'sect283k1', 'sect283r1', 'sect409k1', 'sect409r1', 'sect571k1', 'sect571r1', 'brainpoolP160r1', 'brainpoolP160t1', 'brainpoolP192r1', 'brainpoolP192t1', 'brainpoolP224r1', 'brainpoolP224t1', 'brainpoolP256r1', 'brainpoolP256t1', 'brainpoolP320r1', 'brainpoolP320t1', 'brainpoolP384r1', 'brainpoolP384t1', 'brainpoolP512r1', 'brainpoolP512t1', 'c2pnb163v1', 'c2pnb163v2', 'c2pnb163v3', 'c2pnb176v1', 'c2pnb208w1', 'c2pnb272w1', 'c2pnb304w1', 'c2pnb368w1', 'c2tnb191v1', 'c2tnb191v2', 'c2tnb191v3', 'c2tnb239v1', 'c2tnb239v2', 'c2tnb239v3', 'c2tnb359v1', 'c2tnb431r1', 'wap-wsg-idm-ecid-wtls1', 'wap-wsg-idm-ecid-wtls10', 'wap-wsg-idm-ecid-wtls11', 'wap-wsg-idm-ecid-wtls12', 'wap-wsg-idm-ecid-wtls3', 'wap-wsg-idm-ecid-wtls4', 'wap-wsg-idm-ecid-wtls5', 'wap-wsg-idm-ecid-wtls6', 'wap-wsg-idm-ecid-wtls7', 'wap-wsg-idm-ecid-wtls8', 'wap-wsg-idm-ecid-wtls9'];
 
+let curveName = 'prime256v1';
 for (let curveName of curveNames) {
-      let curveName = 'prime256v1';
       console.log(`------${curveName}------`);
       const ecdh = crypto.createECDH(curveName);
       ecdh.generateKeys();
@@ -10,22 +10,31 @@ for (let curveName of curveNames) {
       let privateKey = ecdh.getPrivateKey(),
             publicKey = ecdh.getPublicKey()
 
-      const pemKeyPair = ecKeyUtils.generatePemKeys(curveName, {privateKey, publicKey});
+      let pem = ecKeyUtils.generatePem(curveName, {privateKey, publicKey});
+      let sk = pem.privateKey;
+      let keyInfo = ecKeyUtils.parseKeyInfo(sk);
+      console.log('Equal Curve?', keyInfo.curveName === curveName);
+      console.log('Equal PrivateKey?', (privateKey.compare(keyInfo.privateKey) === 0) && (keyInfo.privateKey.compare(privateKey) === 0));
+      console.log('Equal PublicKey?', (publicKey.compare(keyInfo.publicKey) === 0) && (keyInfo.publicKey.compare(publicKey) === 0));
 
-      let keyInfo = ecKeyUtils.parseKeyInfo(pemKeyPair.privateKey);
-      console.log('Equal PrivateKey?', privateKey.compare(keyInfo.privateKey) === 0);
-      console.log('Equal PublicKey?', publicKey.compare(keyInfo.publicKey) === 0);
+      pem = ecKeyUtils.generatePem({curveName, privateKey});
+      sk = pem.privateKey;
+      keyInfo = ecKeyUtils.parseKeyInfo(sk);
       console.log('Equal Curve?', keyInfo.curveName === curveName);
-      keyInfo = ecKeyUtils.parseKeyInfo(pemKeyPair.publicKey);
-      console.log('Equal PublicKey?', publicKey.compare(keyInfo.publicKey) === 0);
+      console.log('Equal PrivateKey?', (privateKey.compare(keyInfo.privateKey) === 0) && (keyInfo.privateKey.compare(privateKey) === 0));
+      
+      pem = ecKeyUtils.generatePem({curveName, publicKey});
+      pk = pem.publicKey;
+      keyInfo = ecKeyUtils.parseKeyInfo(pk);
       console.log('Equal Curve?', keyInfo.curveName === curveName);
+      console.log('Equal PublicKey?', (publicKey.compare(keyInfo.publicKey) === 0) && (keyInfo.publicKey.compare(publicKey) === 0));
 
       const sign = crypto.createSign('sha256');
       const message = Buffer.from('Hello, World!');
       sign.update(message);
-      let sig = sign.sign(pemKeyPair.privateKey);
+      let sig = sign.sign(sk);
 
       const verify = crypto.createVerify('sha256');
       verify.update(message);
-      console.log('Singature Passed Verification?', verify.verify(pemKeyPair.publicKey, sig));
+      console.log('Singature Passed Verification?', verify.verify(pk, sig));
 }
